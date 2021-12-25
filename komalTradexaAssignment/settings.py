c5dad1216b59ebf4afb17aae7861f4157a75f64b
unknown <komalnalawade44@gmail.com>
2021-12-25 20:05:11 +0530

    modified contect in new branch

--- komalTradexaAssignment/settings.py
+++ komalTradexaAssignment/settings.py
@@ -139,6 +139,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# LOGIN_REDIRECT_URL='/home'
-# #LOGOUT_REDIRECT_URL='/home'
-# LOGOUT_REDIRECT_URL='/logout'
+LOGIN_REDIRECT_URL='/home'
+#LOGOUT_REDIRECT_URL='/home'
+LOGOUT_REDIRECT_URL='/logout'
